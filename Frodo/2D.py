28f380ddbc803b1e05ccb84731946099bf2650a2
Mismatch-attack-threebears-frodo-round5 <70333313+Mismatch-attack-threebears-frodo-round5@users.noreply.github.com>
2020-08-28 09:19:29 +0200

    Add files via upload

---
+++ Frodo/2D.py
@@ -0,0 +1,151 @@
+import math
+from random import randrange
+import sys
+from BinaryTree2D import *
+
+prob_640 = [1, 4, 17, 56, 164, 422, 958, 1918, 3384, 5264, 7216, 8720, 9288, 8720, 7216, 5264, 3384, 1918, 958, 422, 164, 56, 17, 4, 1]
+prob_976 = [1, 6, 29, 118, 396, 1101, 2545, 4882, 7774, 10277, 11278, 10277, 7774, 4882, 2545, 1101, 396, 118, 29, 6, 1]
+prob_1344 = [2, 40, 364, 2023, 6876, 14320, 18286, 14320, 6876, 2023, 364, 40, 2]
+table_640 = [1, 5, 22, 78, 242, 664, 1622, 3540, 6924, 12188, 19404, 28124, 37412, 46132, 53348, 58612, 61996, 63914, 64872, 65294, 65458, 65514, 65531, 65535]
+table_976 = [1, 7, 36, 154, 550, 1651, 4196, 9078, 16852, 27129, 38407, 48684, 56458, 61340, 63885, 64986, 65382, 65500, 65529, 65535]
+table_1344 = [2, 42, 406, 2429, 9305, 23625, 41911, 56231, 63107, 65130, 65494, 65534]
+max = 65536
+
+def Sample(version):
+    if version == 640:
+        limit = 12
+        table = table_640.copy()
+    elif version == 976:
+        limit = 10
+        table = table_976.copy()
+    elif version == 1344:
+        limit = 6
+        table = table_1344.copy()
+    a = randrange(max)
+    res = -limit
+    for i in range(2*limit):
+        if a >= table[i]:
+            res += 1
+        else:
+            return res
+    return res
+
+def Decode(input, q, B):
+    return int(math.floor(input * (2**B/q) + 1/2)) % 2**B
+
+def Oracle(alphas, targets, q, B):
+    global queries
+    queries += 1
+    C2 = alphas[0]
+    for i in range(len(targets)):
+        C2 -= alphas[i+1] * targets[i]
+    C2 = C2 % q
+    dec = Decode(C2, q, B)
+    if dec == 0:
+        return '+'
+    else:
+        return '-'
+
+def RecoverCoefficient(root, sk, i, j, version):
+    if version == 640:
+        q = 32768
+        B = 2
+    elif version == 976:
+        q = 65536
+        B = 3
+    elif version == 1344:
+        q = 65536
+        B = 4
+    while root.left != None:
+        alphas = [root.value, root.alpha, root.beta]
+        targets = [sk[i][j],sk[i+1][j]]
+        t = Oracle(alphas, targets, q, B)
+        if  t == '-':
+            root = root.left
+        else:
+            root = root.right
+    return root.alpha, root.beta
+
+
+
+
+
+
+
+root = reconstruct(readjson("data\Frodo640_2D.txt"))
+total = 100
+correct = 0
+queries = 0
+version = 640
+for k in range(total):
+    sk = 640*[8*[0]]
+    for i in range(640):
+        for j in range(8):
+            sk[i][j] = Sample(version)
+    recover = 640*[8*[0]]
+    for i in range(0,640,2):
+        for j in range(8):
+            recover[i][j], recover[i+1][j] = RecoverCoefficient(root, sk, i, j, 640)
+    if recover == sk:
+        correct += 1
+print('-----------------------------------------------------------------------')
+print('Targeting Frodo640, dimension of the attack is 2:')
+print()
+print('Average success probability: ' + str(correct/total))
+print('Average number of queries to recover 1 secret coefficients: ' + str(queries/(640*8*total)))
+print('Average number of queries to recover the whole secret key: ' + str(queries/total))
+print('-----------------------------------------------------------------------')
+print()
+print()
+
+
+root = reconstruct(readjson("data\Frodo976_2D.txt"))
+total = 100
+correct = 0
+queries = 0
+version = 976
+for k in range(total):
+    sk = version*[8*[0]]
+    for i in range(version):
+        for j in range(8):
+            sk[i][j] = Sample(version)
+    recover = version*[8*[0]]
+    for i in range(0,version,2):
+        for j in range(8):
+            recover[i][j], recover[i+1][j] = RecoverCoefficient(root, sk, i, j, version)
+    if recover == sk:
+        correct += 1
+print('-----------------------------------------------------------------------')
+print('Targeting Frodo976, dimension of the attack is 2:')
+print()
+print('Average success probability: ' + str(correct/total))
+print('Average number of queries to recover 1 secret coefficients: ' + str(queries/(976*8*total)))
+print('Average number of queries to recover the whole secret key: ' + str(queries/total))
+print('-----------------------------------------------------------------------')
+print()
+print()
+
+root = reconstruct(readjson("data\Frodo1344_2D.txt"))
+total = 100
+correct = 0
+queries = 0
+version = 1344
+for k in range(total):
+    sk = version*[8*[0]]
+    for i in range(version):
+        for j in range(8):
+            sk[i][j] = Sample(version)
+    recover = version*[8*[0]]
+    for i in range(0,version,2):
+        for j in range(8):
+            recover[i][j], recover[i+1][j] = RecoverCoefficient(root, sk, i, j, version)
+    if recover == sk:
+        correct += 1
+print('Targeting Frodo1344, dimension of the attack is 2:')
+print()
+print('Average success probability: ' + str(correct/total))
+print('Average number of queries to recover 1 secret coefficients: ' + str(queries/(1344*8*total)))
+print('Average number of queries to recover the whole secret key: ' + str(queries/total))
+print('-----------------------------------------------------------------------')
+print()
+print()
---
+++ Frodo/BinaryTree2D.py
@@ -0,0 +1,94 @@
+#! /usr/bin/python
+
+import json
+
+# for leaves, value is the secret coeff
+class Node:
+    def __init__(self, id, left, right, value, alpha, beta):
+        self.id = id
+        self.left = left
+        self.right = right
+        self.value = value
+        self.alpha = alpha
+        self.beta = beta
+    """
+    def __str__(self):
+        choices = "coeff=" + str(self.coeff) + "; \t l_j=[" + str(self.j0) + "," + str(self.j1) + "," + str(
+            self.j2) + "," + str(self.j3) + "]" if self.left else "S_j=[" + str(self.j0) + "," + str(
+            self.j1) + "," + str(self.j2) + "," + str(self.j3) + "]"
+        children = "(" + (str(self.left.id) if self.left else "x") + "," + (
+            str(self.right.id) if self.right else "x") + "),  \t" if self.left else " =>\t"
+        return (str(self.id) + ": \t" + children + choices)
+    """
+
+def Atojson(node):
+    return ([node.id, node.left.id, node.right.id, node.value, node.alpha, node.beta] if node.left else [
+        node.id, None, None, node.value, node.alpha, node.beta])
+
+
+length = 0
+treearray = []
+idcount = 0
+
+def newnode(left, right, value, alpha, beta):
+    global length
+    length = length + 1
+    node = Node(length, left, right, value, alpha, beta)
+    return (node)
+
+def newnodefromarray(node):
+    result = newnode(None, None, node[3], node[4], node[5])
+    return (result)
+
+def reconstruct(nodes):
+    global length
+    length = 0
+    nodearray = []
+    for i in range(len(nodes)):
+        nodearray.append(newnodefromarray(nodes[i]))
+    for i in range(len(nodes)):
+        nodearray[i].left = nodearray[nodes[i][1] - 1] if nodes[i][1] else None
+        nodearray[i].right = nodearray[nodes[i][2] - 1] if nodes[i][2] else None
+    return (nodearray[len(nodes) - 1])
+
+def treetoarray(node):
+    global treearray
+    if (node.left == None):
+        treearray.append(Atojson(node))
+    else:
+        treetoarray(node.left)
+        treetoarray(node.right)
+        treearray.append(Atojson(node))
+
+def writejson(obj, filename):
+    global treearray
+    global idcount
+    idcount = 1
+    addid(obj)
+    treearray = []
+    treetoarray(obj)
+    with open(filename, "w") as output:
+        json.dump(treearray, output)
+
+def readjson(filename):
+    with open(filename, "rb") as input:
+        return (json.load(input))
+
+def printnodes(node):
+    if (node.left == None):
+        print(node)
+    else:
+        printnodes(node.left)
+        printnodes(node.right)
+        print(node)
+
+def addid(node):
+    global idcount
+    if(node.left==None):
+        node.id=idcount
+        idcount=idcount+1
+    else:
+        addid(node.left)
+        addid(node.right)
+        node.id=idcount
+        idcount=idcount+1
